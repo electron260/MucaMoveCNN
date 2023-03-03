e704e1fec20eafe6ee72132eaa5b1faa9f7289bb
HugoD <hugo.daumain@edu.devinci.fr>
2023-03-03 13:28:10 +0100

    clean

---
+++ esp32muca/.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
+ /home/hugo/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ /home/hugo/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ /home/hugo/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ esp32muca/.pio/build/esp32doit-devkit-v1/libf5e/Adafruit BME680 Library/bme68x.c.d
@@ -0,0 +1,4 @@
+.pio/build/esp32doit-devkit-v1/libf5e/Adafruit\ BME680\ Library/bme68x.c.o: \
+ .pio/libdeps/esp32doit-devkit-v1/Adafruit\ BME680\ Library/bme68x.c \
+ .pio/libdeps/esp32doit-devkit-v1/Adafruit\ BME680\ Library/bme68x.h \
+ .pio/libdeps/esp32doit-devkit-v1/Adafruit\ BME680\ Library/bme68x_defs.h
